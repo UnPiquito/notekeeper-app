d65f41ccdf9d6e0f52d7f85b1735d6e3f4090334
Unai Velo de los Santos <unai.velo@mataro.epiaedu.cat>
2024-06-11 15:33:54 +0200

    Last_Commit_Nº22

--- src/components/UpdateNoteFrom/UpdateNoteFrom.jsx
+++ src/components/UpdateNoteFrom/UpdateNoteFrom.jsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { useState } from "react";
-=======
->>>>>>> ddff1c28f8566ad821ca3a9bec5a4a4df99ad0c1
 import noteService from "../../services/notes/noteService.js";
 
-<<<<<<< HEAD
 const UpdateNoteForm = ({ noteId, visible }) => {
-=======
-const UpdateNoteForm = ({ visible }) => {
-  const {updateNote} = useNote();
->>>>>>> ddff1c28f8566ad821ca3a9bec5a4a4df99ad0c1
   const [note, setNote] = useState({
     id: "",
     name: "",
